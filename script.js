ddb105a76a6cb896c82658ba39ccc974f974cdb2
TsukiharaYuki64 <ebrizuela1945@gmail.com>
2024-07-03 23:30:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const interestsChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: ['Gaming', 'Coding', 'Entertainment', 'Research', 'Anime'],
+            labels: ['Gaming', 'Coding', 'Entertainment', 'Research', 'Communication', 'Anime'],
             datasets: [{
-                data: [10, 30, 20, 30, 10], // Percentages for each activity
+                data: [10, 30, 20, 30, 10, 15], // Percentages for each activity
                 backgroundColor: [
                     'rgba(0, 255, 0, 0.2)', // Lighter green
                     'rgba(0, 128, 0, 0.2)', // Darker green
                     'rgba(0, 255, 0, 0.4)', // Lighter green
                     'rgba(0, 128, 0, 0.4)', // Darker green
-                    'rgba(0, 255, 0, 0.6)'  // Lighter green
+                    'rgba(0, 255, 0, 0.6)', // Lighter green
+                    'rgba(0, 128, 0, 0.6)'  // Darker green
                 ],
                 borderColor: [
+                    'rgba(0, 255, 0, 1)',   // Green
                     'rgba(0, 255, 0, 1)',   // Green
                     'rgba(0, 255, 0, 1)',   // Green
                     'rgba(0, 255, 0, 1)',   // Green
